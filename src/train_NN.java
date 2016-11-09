d8e3c2f38dd6e0b1c1f0dd8a49598386c14ab7a3
davidzomerdijk <david.zomerdijk93@gmail.com>
2016-11-09 13:32:28 +0100

    added new training set

--- src/train_NN.java
+++ src/train_NN.java
@@ -14,22 +14,21 @@ public class train_NN {
 
     public static void main(String[] args) {
 
-        DataSet trainingSet = DataSet.createFromFile("./train_data/aalborg4.csv",19, 1, ",", true);
-        //System.out.println(trainingSet.toString());
+        DataSet trainingSet = DataSet.createFromFile("./train_data/aalborg4.csv", 19, 1, ",", true);
+        System.out.println(trainingSet.toString());
         SupervisedHebbianNetwork NN_acc = new SupervisedHebbianNetwork( 19, 1);
         System.out.println("start learning");
         NN_acc.learn(trainingSet);
 
         System.out.println("the code has run");
 
-        for(DataSetRow dataRow : trainingSet.getRows() ) {
-            NN_acc.setInput(dataRow.getInput());
-            NN_acc.calculate();
-            double[ ] networkOutput = NN_acc.getOutput();
-            System.out.println("data: " + Arrays.toString(dataRow.getDesiredOutput() ));
-            System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
-            System.out.println(" Output: " + Arrays.toString(networkOutput) );
-        }
+      for(DataSetRow dataRow : trainingSet.getRows() ) {NN_acc.setInput(dataRow.getInput());
+          NN_acc.calculate();
+          double[ ] networkOutput = NN_acc.getOutput();
+          System.out.println("data: " + Arrays.toString(dataRow.getDesiredOutput() ));
+          System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
+          System.out.println(" Output: " + Arrays.toString(networkOutput) );
+      }
 
     }
 
