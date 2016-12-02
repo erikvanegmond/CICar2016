dbb3f702246589b2a7abc8fe82cac792c3955030
Erik van Egmond <erikvane@gmail.com>
2016-12-02 14:13:21 +0100

    Added a constants file for constants we can reuse like file names

---
+++ src/Const.java
@@ -0,0 +1,9 @@
+/**
+ * Created by erikv on 2-12-2016.
+ */
+public class Const {
+
+    final static String ALL_NN_FNAME = "./trained_models/all_NN";
+    final static String ALL_TRAIN_SET = "./train_data/trainingset_all";
+
+}
